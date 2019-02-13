282178df482df6d805bba083a4a4d9f4f971696c
Jeff Dairiki <dairiki@dairiki.org>
2019-02-12 18:41:57 -0800

    Add restart option

--- bh-hide-rats.py
+++ bh-hide-rats.py
@@ -3,11 +3,11 @@
 ''' Randomize the position of selected elements
 
 '''
-# standard library
 import collections
+from functools import update_wrapper
 from itertools import count
 import random
-# local library
+
 import inkex
 import simpletransform
 
@@ -23,14 +23,14 @@ def debug(message):
     inkex.errormsg(message)
 
 
-_bounds = collections.namedtuple('_bounds', ['xmin', 'xmax', 'ymin', 'ymax'])
+_BBoxBase = collections.namedtuple('_bounds', ['xmin', 'xmax', 'ymin', 'ymax'])
 
 
-class bounds(_bounds):
+class BBox(_BBoxBase):
     def __new__(typ, *args):
         if len(args) == 1:
             args = args[0]
-        return _bounds.__new__(typ, *args)
+        return _BBoxBase.__new__(typ, *args)
 
     @property
     def width(self):
@@ -63,20 +63,91 @@ def context_transform(el, map=[[1, 0, 0], [0, 1, 0]]):
     return map
 
 
-def randomize_position(el, bbox, exclusions, max_tries=32):
-    """ Randomize the position of element within bounding box.
-    """
-    # Work in page coordinates
-    local_trfm = context_transform(el)
-    el_bbox = bounds(simpletransform.computeBBox([el], local_trfm))
+E = [[1, 0, 0], [0, 1, 0]]
 
-    xmax = max(bbox.xmax - el_bbox.width, bbox.xmin)
-    ymax = max(bbox.ymax - el_bbox.height, bbox.ymin)
+
+class reify(object):
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        update_wrapper(self, wrapped)
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
+
+
+class Rat(object):
+    """ Helper for element placement """
+    def __init__(self, element):
+        self.element = element
+
+    @reify
+    def parent_transform(self):
+        parent = self.element.getparent()
+        mat = E
+        while parent is not None:
+            trans = parent.get('transform')
+            if trans:
+                trans = simpletransform.parseTransform(trans)
+                mat = simpletransform.composeTransform(trans, mat)
+            parent = parent.getparent()
+        return mat
+
+    @reify
+    def bbox(self):
+        return BBox(simpletransform.computeBBox([self.element],
+                                                self.parent_transform))
+
+    def _clear_cache(self):
+        del self.parent_transform
+        del self.bbox
+
+    @property
+    def width(self):
+        bbox = self.bbox
+        return bbox.xmax - bbox.xmin
+
+    @property
+    def height(self):
+        bbox = self.bbox
+        return bbox.ymax - bbox.ymin
+
+    @property
+    def position(self):
+        bbox = self.bbox
+        return bbox.xmin, bbox.ymin
+
+    @position.setter
+    def position(self, (newx, newy)):
+        x, y = self.position
+        offset = [newx - x, newy - y]
+        inv_trfm = simpletransform.invertTransform(self.parent_transform)
+        simpletransform.applyTransformToPoint(inv_trfm, offset)
+        local_trfm = [[1, 0, offset[0]],
+                      [0, 1, offset[1]]]
+        simpletransform.applyTransformToNode(local_trfm, self.element)
+        self._clear_cache()
+
+
+def random_position(bbox, bounds, exclusions=[], max_tries=32):
+    """Find a random new position for element.
+
+    The element has dimensions given by BBOX.  The new position will
+    be contained within BOUNDS, if possible.  Reasonable efforts will
+    be made to avoid placing the element such that it overlaps with
+    any bboxes listed in EXCLUSIONS.
+
+    """
+    xmax = max(bounds.xmax - bbox.width, bounds.xmin)
+    ymax = max(bounds.ymax - bbox.height, bounds.ymin)
     for n in count(1):
         # Compute random position offset
-        x = random.uniform(bbox.xmin, xmax)
-        y = random.uniform(bbox.ymin, ymax)
-        new_bbox = bounds(x, x + el_bbox.width, y, y + el_bbox.height)
+        x = random.uniform(bounds.xmin, xmax)
+        y = random.uniform(bounds.ymin, ymax)
+        new_bbox = BBox(x, x + bbox.width, y, y + bbox.height)
         if not any(new_bbox.overlaps(excl) for excl in exclusions):
             break
         elif n > max_tries:
@@ -84,27 +155,37 @@ def randomize_position(el, bbox, exclusions, max_tries=32):
                 "Can not find non-excluded location for rat after %d tries. "
                 "Giving up." % n)
             break
+    return x, y
 
-    offset = [x - el_bbox.xmin, y - el_bbox.ymin]
-    # Transform back to element coordinates
-    local_trfm_inv = simpletransform.invertTransform(local_trfm)
-    simpletransform.applyTransformToPoint(local_trfm_inv, offset)
-    trfm = [[1, 0, offset[0]],
-            [0, 1, offset[1]]]
-    simpletransform.applyTransformToNode(trfm, el)
+
+def make_auto_exclusion(bbox):
+    excl = inkex.etree.Element(
+        inkex.addNS('rect', 'svg'),
+        x="%f" % bbox.xmin,
+        y="%f" % bbox.ymin,
+        width="%f" % bbox.width,
+        height="%f" % bbox.height,
+        # style='display:none;fill:#9bff00;stroke:#ff0000ff;stroke-width:1px',
+        style='fill:#9bff0035;fill-opacity:0.2;stroke:#ff0000;stroke-width:1px',
+        )
+    excl.set("{%s}rat-exclusion" % NSMAP['bh'], 'auto')
+    # "lock" the exclusion
+    excl.set("{%s}insensitive" % NSMAP['sodipodi'], 'true')
+    return excl
 
 
 class HideRats(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
         self.OptionParser.add_option("--tab")
+        self.OptionParser.add_option("--restart", type="inkbool")
         self.OptionParser.add_option("--verbose", type="inkbool")
 
     def get_page_boundary(self):
         svg = self.document.getroot()
         xmax = self.unittouu(svg.attrib['width'])
         ymax = self.unittouu(svg.attrib['height'])
-        return bounds(0, xmax, 0, ymax)
+        return BBox(0, xmax, 0, ymax)
 
     def get_boundary(self):
         document = self.document
@@ -116,22 +197,57 @@ class HideRats(inkex.Effect):
 
         if bbox is None:
             bbox = self.get_page_boundary()
-        return bounds(bbox)
+        return BBox(bbox)
 
     def get_exclusions(self):
         document = self.document
         exclusions = []
         for el in document.xpath('//*[@bh:rat-exclusion]', namespaces=NSMAP):
-            trfm = context_transform(el)
-            bbox = simpletransform.computeBBox([el], trfm)
-            exclusions.append(bounds(bbox))
+            exclusions.append(Rat(el).bbox)
         return exclusions
 
+    @reify
+    def exclusion_layer(self):
+        document = self.document
+        r = document.xpath("//svg:g[@bh:rat-exclusion-layer]",
+                           namespaces=NSMAP)
+        if r:
+            return r[0]
+        g = inkex.etree.Element(
+            inkex.addNS('g', 'svg'))
+        g.set("{%s}rat-exclusion-layer" % NSMAP['bh'], 'true')
+        g.set("{%s}groupmode" % NSMAP['inkscape'], 'layer')
+        g.set("{%s}label" % NSMAP['inkscape'], '[h] ' + _('Rat auto-exclusions'))
+        # "lock" layer
+        g.set("{%s}insensitive" % NSMAP['sodipodi'], 'true')
+        document.getroot().append(g)
+        return g
+
+    def cleanup_exclusions(self):
+        document = self.document
+        for el in document.xpath("//svg:g[@bh:rat-exclusion-layer]",
+                                 namespaces=NSMAP):
+            el.getparent().remove(el)
+
     def effect(self):
-        bbox = self.get_boundary()
+        if self.options.restart:
+            self.cleanup_exclusions()
+
+        bounds = self.get_boundary()
         exclusions = self.get_exclusions()
-        for el in self.selected.values():
-            randomize_position(el, bbox, exclusions)
+
+        rats = [Rat(el) for el in self.selected.values()]
+        for rat in rats:
+            # Add an exclusion rectangle for the current rat position
+            if not any(rat.bbox.overlaps(excl) for excl in exclusions):
+                self.exclusion_layer.append(make_auto_exclusion(rat.bbox))
+            exclusions.append(rat.bbox)
+
+        for rat in rats:
+            newpos = random_position(rat.bbox, bounds, exclusions)
+            rat.position = newpos
+            self.exclusion_layer.append(make_auto_exclusion(rat.bbox))
+            exclusions.append(rat.bbox)
 
 
 if __name__ == '__main__':
