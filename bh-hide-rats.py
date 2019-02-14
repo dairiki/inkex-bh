4a45dffc7ca7a57c0fbc343bae26e14abcf0dd2d
Jeff Dairiki <dairiki@dairiki.org>
2019-02-13 16:52:11 -0800

    Refactor how exclusions work.
    Now all is copied to the temporary layer on the first run.
    Then only the elements in the temporary "guide" layer matter.
    These may be edited by the user if so desired.

--- bh-hide-rats.py
+++ bh-hide-rats.py
@@ -16,8 +16,18 @@ import simpletransform
 
 inkex.localize()
 
+SVG_SVG = inkex.addNS('svg', 'svg')
+SVG_G = inkex.addNS('g', 'svg')
+SVG_USE = inkex.addNS('use', 'svg')
+SVG_RECT = inkex.addNS('rect', 'svg')
+INKSCAPE_GROUPMODE = inkex.addNS('groupmode', 'inkscape')
+INKSCAPE_LABEL = inkex.addNS('label', 'inkscape')
+SODIPODI_INSENSTIVE = inkex.addNS('insensitive', 'sodipodi')
+XLINK_HREF = inkex.addNS('href', 'xlink')
+
 BH_NS = 'http://dairiki.org/barnhunt/inkscape-extensions'
 BH_RAT_EXCLUSION = etree.QName(BH_NS, 'rat-exclusion')
+BH_RAT_GUIDE_MODE = etree.QName(BH_NS, 'rat-guide-mode')
 
 NSMAP = inkex.NSS.copy()
 NSMAP['bh'] = BH_NS
@@ -208,6 +218,87 @@ class Element(object):
                 delattr(self, name)
 
 
+class RatGuide(object):
+    def __init__(self, document, page_bbox):
+        self.document = document
+        self.page_bbox = page_bbox
+
+        existing = document.xpath("//svg:g[@bh:rat-guide-mode='layer']",
+                                  namespaces=NSMAP)
+        if existing:
+            self.guide_layer = existing[0]
+        else:
+            self.guide_layer = self._create_guide_layer()
+            self._populate_guide_layer()
+
+    def _create_guide_layer(self):
+        layer = inkex.etree.Element(SVG_G)
+        layer.attrib.update({
+            INKSCAPE_LABEL: '[h] %s' % _('Rat Placement Guides'),
+            INKSCAPE_GROUPMODE: 'layer',
+            BH_RAT_GUIDE_MODE: 'layer',
+            SODIPODI_INSENSTIVE: 'true',
+            })
+        self.document.getroot().append(layer)
+        return layer
+
+    def _populate_guide_layer(self):
+        # Draw bounding box on guide layer
+        bounds = make_rect(self._get_boundary())
+        bounds.attrib.update({
+            BH_RAT_GUIDE_MODE: 'boundary',
+            'style': 'fill:#00ff00;fill-opacity:0.02;stroke:#00ab00:stroke-width:2.5;stroke-linecap:round;stroke-miterlimit:4',
+            })
+        self.guide_layer.append(bounds)
+
+        # Find visible exclusion elements and draw their bounding boxes
+        for bbox in find_exclusions(self.document.getroot()):
+            # FIXME: add link to original exclusion element?
+            self.add_exclusion(bbox)
+
+    def _get_boundary(self):
+        return self._compute_boundary(
+            self.document.xpath('//*[@bh:rat-boundary]', namespaces=NSMAP))
+
+    def _compute_boundary(self, elems):
+        bbox = None
+        for el in elems:
+            boundary = Element(el)
+            if bbox is None:
+                bbox = boundary.bbox
+            else:
+                bbox += boundary.bbox
+        if bbox is None:
+            bbox = self.page_bbox
+        return bbox
+
+    def reset(self):
+        guide_layer = self.guide_layer
+        while len(guide_layer) > 0:
+            guide_layer.remove(guide_layer[0])
+        self._populate_guide_layer()
+
+    def add_exclusion(self, bbox, src_id=None):
+        rect = make_rect(bbox)
+        rect.attrib.update({
+            BH_RAT_GUIDE_MODE: 'exclusion',
+            'style': 'fill:#c68c8c;fill-opacity:0.25;stroke:#ff0000:stroke-width:1;stroke-linecap:round;stroke-miterlimit:4',
+            })
+        if src_id is not None:
+            rect.set(XLINK_HREF, '#%s' % src_id)
+        self.guide_layer.append(rect)
+
+    def get_boundary(self):
+        return self._compute_boundary(
+            self.guide_layer.xpath(".//*[@bh:rat-guide-mode='boundary']",
+                                   namespaces=NSMAP))
+
+    def get_exclusions(self):
+        res = self.guide_layer.xpath(".//*[@bh:rat-guide-mode='exclusion']",
+                                     namespaces=NSMAP)
+        return [Element(excl).bbox for excl in res]
+
+
 def random_position(dim, bounds, exclusions=[], max_tries=128):
     """Find a random new position for element.
 
@@ -255,8 +346,8 @@ def find_exclusions(elem, transform=None):
         if el.get(BH_RAT_EXCLUSION) is not None:
             exclusions.append(Element(el).compute_bbox(transform))
         else:
-            assert el.tag == inkex.addNS('use', 'svg')
-            href = el.get(inkex.addNS('href', 'xlink'))
+            assert el.tag == SVG_USE
+            href = el.get(XLINK_HREF)
             assert href.startswith('#') and len(href) > 1
 
             local_tfm = composed_transform(Element(el), transform)
@@ -265,20 +356,24 @@ def find_exclusions(elem, transform=None):
     return exclusions
 
 
+def make_rect(bbox):
+    return inkex.etree.Element(SVG_RECT,
+                               x="%f" % bbox.xmin,
+                               y="%f" % bbox.ymin,
+                               width="%f" % bbox.width,
+                               height="%f" % bbox.height)
+
+
 def make_auto_exclusion(bbox):
-    excl = inkex.etree.Element(
-        inkex.addNS('rect', 'svg'),
-        x="%f" % bbox.xmin,
-        y="%f" % bbox.ymin,
-        width="%f" % bbox.width,
-        height="%f" % bbox.height,
+    elem = make_rect(bbox)
+    elem.attrib.update({
         # style='display:none;fill:#9bff00;stroke:#ff0000ff;stroke-width:1px',
-        style='fill:#b3c895;fill-opacity:0.25;stroke:#ff0000;stroke-width:1px',
-        )
-    excl.set("{%s}rat-exclusion" % NSMAP['bh'], 'auto')
-    # "lock" the exclusion
-    excl.set("{%s}insensitive" % NSMAP['sodipodi'], 'true')
-    return excl
+        'style': 'fill:#b3c895;fill-opacity:0.25;stroke:#ff0000;stroke-width:1px',
+        BH_RAT_EXCLUSION: 'auto',
+        # "lock" the exclusion
+        SODIPODI_INSENSTIVE: 'true',
+        })
+    return elem
 
 
 class HideRats(inkex.Effect):
@@ -294,67 +389,29 @@ class HideRats(inkex.Effect):
         ymax = self.unittouu(svg.attrib['height'])
         return BoundingBox(0, xmax, 0, ymax)
 
-    def get_boundary(self):
-        document = self.document
-        bbox = None
-        for el in document.xpath('//*[@bh:rat-boundary]', namespaces=NSMAP):
-            boundary = Element(el)
-            if bbox is None:
-                bbox = boundary.bbox
-            else:
-                bbox += boundary.bbox
-        if bbox is None:
-            bbox = self.get_page_boundary()
-        return bbox
-
-    def get_exclusions(self):
-        return find_exclusions(self.document.getroot())
-
-    @reify
-    def exclusion_layer(self):
-        document = self.document
-        r = document.xpath("//svg:g[@bh:rat-exclusion-layer]",
-                           namespaces=NSMAP)
-        if r:
-            return r[0]
-        g = inkex.etree.Element(
-            inkex.addNS('g', 'svg'))
-        g.set("{%s}rat-exclusion-layer" % NSMAP['bh'], 'true')
-        g.set("{%s}groupmode" % NSMAP['inkscape'], 'layer')
-        g.set("{%s}label" % NSMAP['inkscape'], '[h] ' + _('Rat auto-exclusions'))
-        # "lock" layer
-        g.set("{%s}insensitive" % NSMAP['sodipodi'], 'true')
-        document.getroot().append(g)
-        return g
-
-    def cleanup_exclusions(self):
-        document = self.document
-        for el in document.xpath("//svg:g[@bh:rat-exclusion-layer]",
-                                 namespaces=NSMAP):
-            el.getparent().remove(el)
-
     def effect(self):
+        guide_layer = RatGuide(self.document, self.get_page_boundary())
+
         if self.options.restart:
-            self.cleanup_exclusions()
+            # FIXME:
+            guide_layer.reset()
 
-        bounds = self.get_boundary()
-        exclusions = self.get_exclusions()
-
-        # FIXME: only do this for exclusions which are not visible?
-        # for excl in exclusions:
-        #     self.exclusion_layer.append(make_auto_exclusion(excl))
+        bounds = guide_layer.get_boundary()
+        exclusions = guide_layer.get_exclusions()
 
         rats = [Element(el) for el in self.selected.values()]
         for rat in rats:
             # Add an exclusion rectangle for the current rat position
             if not any(rat.bbox.overlaps(excl) for excl in exclusions):
-                self.exclusion_layer.append(make_auto_exclusion(rat.bbox))
-            exclusions.append(rat.bbox)
+                # FIXME: link to original rat id
+                guide_layer.add_exclusion(rat.bbox)
+                # FIXME: shouldn't need this
+                exclusions.append(rat.bbox)
 
         for rat in rats:
             newpos = random_position(rat.bbox.dimension, bounds, exclusions)
             rat.position = newpos
-            self.exclusion_layer.append(make_auto_exclusion(rat.bbox))
+            guide_layer.add_exclusion(rat.bbox)
             exclusions.append(rat.bbox)
 
 
