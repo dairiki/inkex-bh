ef1a4f5352afe7b474ae3d9f7ccda0d70d327332
Jeff Dairiki <dairiki@dairiki.org>
2019-02-14 10:17:43 -0800

    Big refactor.

--- bh-hide-rats.py
+++ bh-hide-rats.py
@@ -3,10 +3,12 @@
 ''' Randomize the position of selected elements
 
 '''
-import collections
+from collections import namedtuple
 from functools import update_wrapper
 from itertools import count
 import random
+
+from lxml import etree
 
 import inkex
 import simpletransform
@@ -14,19 +16,44 @@ import simpletransform
 
 inkex.localize()
 
+BH_NS = 'http://dairiki.org/barnhunt/inkscape-extensions'
+BH_RAT_EXCLUSION = etree.QName(BH_NS, 'rat-exclusion')
 
 NSMAP = inkex.NSS.copy()
-NSMAP['bh'] = 'http://dairiki.org/barnhunt/inkscape-extensions'
+NSMAP['bh'] = BH_NS
 
 
-_BBoxBase = collections.namedtuple('_bounds', ['xmin', 'xmax', 'ymin', 'ymax'])
-
-
-class BBox(_BBoxBase):
-    def __new__(typ, *args):
-        if len(args) == 1:
+class Point(namedtuple('Point', ['x', 'y'])):
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1 and not kwargs:
             args = args[0]
-        return _BBoxBase.__new__(typ, *args)
+        return super(Point, cls).__new__(cls, *args, **kwargs)
+
+    def __sub__(self, other):
+        if not isinstance(other, Point):
+            raise TypeError()
+        return Offset(dx=self.x - other.x, dy=self.y - other.y)
+
+
+class Offset(namedtuple('Offset', ['dx', 'dy'])):
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1 and not kwargs:
+            args = args[0]
+        return super(Offset, cls).__new__(cls, *args, **kwargs)
+
+
+class Dimension(namedtuple('Point', ['width', 'height'])):
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1 and not kwargs:
+            args = args[0]
+        return super(Dimension, cls).__new__(cls, *args, **kwargs)
+
+
+class BoundingBox(namedtuple('BoundingBox', ['xmin', 'xmax', 'ymin', 'ymax'])):
+    def __new__(cls, *args, **kwargs):
+        if len(args) == 1 and not kwargs:
+            args = args[0]
+        return super(BoundingBox, cls).__new__(cls, *args, **kwargs)
 
     @property
     def width(self):
@@ -36,14 +63,75 @@ class BBox(_BBoxBase):
     def height(self):
         return self.ymax - self.ymin
 
+    @property
+    def dimension(self):
+        return Dimension(width=self.xmax - self.xmin,
+                         height=self.ymax - self.ymin)
+
+    @property
+    def ul(self):
+        return Point(x=self.xmin, y=self.ymin)
+
+    @property
+    def lr(self):
+        return Point(x=self.xmax, y=self.ymax)
+
+    def __sum__(self, other):
+        if not isinstance(other, BoundingBox):
+            raise TypeError()
+        return BoundingBox(simpletransform.boxunion(self, other))
+
     def overlaps(self, other):
         return (self.xmin <= other.xmax
                 and self.ymin <= other.ymax
                 and other.xmin <= self.xmax
                 and other.ymin <= self.ymax)
 
+
+class Transform(tuple):
+    def __new__(cls, transform=None):
+        if transform is None:
+            transform = ((1, 0, 0), (0, 1, 0))
+        elif isinstance(transform, basestring):
+            transform = simpletransform.parseTransform(transform)
+        elif isinstance(transform, Transform):
+            # FIXME: better type checking
+            pass
+        return tuple.__new__(cls, transform)
+
+    @classmethod
+    def offset(cls, offset):
+        offset = Offset(offset)
+        return cls(((1, 0, offset.dx),
+                    (0, 1, offset.dy)))
+
+    def inverse(self):
+        return Transform(simpletransform.invertTransform(self))
+
+    def __mul__(self, other):
+        if isinstance(other, Transform):
+            return Transform(
+                simpletransform.composeTransform(self, other))
+        elif isinstance(other, Point):
+            (a11, a12, a13), (a21, a22, a23) = self
+            return Point(x=a11 * other.x + a12 * other.y + a13,
+                         y=a21 * other.x + a22 * other.y + a23)
+        elif isinstance(other, Offset):
+            (a11, a12, _), (a21, a22, _) = self
+            return Offset(dx=a11 * other.dx + a12 * other.dy,
+                          dy=a21 * other.dx + a22 * other.dy)
+        else:
+            raise TypeError()
+
+    def __str__(self):
+        (a11, a12, a13), (a21, a22, a23) = self
+        if (a11, a12, a21, a22) == (1, 0, 0, 1):
+            return "translate(%f,%f)" % (a13, a23)
+        else:
+            return simpletransform.formatTransform(self)
+
     def __repr__(self):
-        return tuple.__repr__(self)
+        return "%s%r" % (self.__class__.__name__, tuple(self))
 
 
 class reify(object):
@@ -59,73 +147,83 @@ class reify(object):
         return val
 
 
-# FIXME: rename this
-class Rat(object):
+def lineage(elem):
+    while elem:
+        yield elem
+        elem = elem.parent
+
+
+def composed_transform(elem, transform=None):
+    return reduce(lambda transform, el: el.transform * transform,
+                  lineage(elem),
+                  Transform(transform))
+
+
+class Element(object):
     """ Helper for element placement """
     def __init__(self, element):
         self.element = element
 
-    @reify
-    def parent_transform(self):
+    @property
+    def transform(self):
+        return Transform(self.element.get('transform'))
+
+    @transform.setter
+    def transform(self, newval):
+        if not isinstance(newval, basestring):
+            newval = str(Transform(newval))
+        return self.element.set('transform', newval)
+
+    @property
+    def parent(self):
         parent = self.element.getparent()
-        mat = [[1, 0, 0], [0, 1, 0]]
-        while parent is not None:
-            trans = parent.get('transform')
-            if trans:
-                trans = simpletransform.parseTransform(trans)
-                mat = simpletransform.composeTransform(trans, mat)
-            parent = parent.getparent()
-        return mat
+        if parent is not None:
+            return Element(parent)
+
+    def compute_bbox(self, transform=None):
+        t = composed_transform(self.parent, transform)
+        return BoundingBox(simpletransform.computeBBox([self.element], t))
 
     @reify
     def bbox(self):
-        return BBox(simpletransform.computeBBox([self.element],
-                                                self.parent_transform))
-
-
-    # @property
-    # def width(self):
-    #     bbox = self.bbox
-    #     return bbox.xmax - bbox.xmin
-
-    # @property
-    # def height(self):
-    #     bbox = self.bbox
-    #     return bbox.ymax - bbox.ymin
+        return self.compute_bbox()
 
     @property
     def position(self):
-        bbox = self.bbox
-        return bbox.xmin, bbox.ymin
+        return self.bbox.ul
 
     @position.setter
-    def position(self, (newx, newy)):
-        x, y = self.position
-        offset = [newx - x, newy - y]
-        inv_trfm = simpletransform.invertTransform(self.parent_transform)
-        simpletransform.applyTransformToPoint(inv_trfm, offset)
-        local_trfm = [[1, 0, offset[0]],
-                      [0, 1, offset[1]]]
-        simpletransform.applyTransformToNode(local_trfm, self.element)
-        del self.bbox           # delete cached value
+    def position(self, newpos):
+        offset = Point(newpos) - self.position
+        local_offset = composed_transform(self.parent).inverse() * offset
+        self.transform = Transform.offset(local_offset) * self.transform
+        self._clear_cache()
+
+    def _clear_cache(self):
+        """Delete cached values."""
+        # Reset all reified values
+        cls = self.__class__
+        for name in dir(cls):
+            if isinstance(getattr(cls, name), reify):
+                delattr(self, name)
 
 
-def random_position(bbox, bounds, exclusions=[], max_tries=32):
+def random_position(dim, bounds, exclusions=[], max_tries=128):
     """Find a random new position for element.
 
-    The element has dimensions given by BBOX.  The new position will
+    The element has dimensions given by DIM.  The new position will
     be contained within BOUNDS, if possible.  Reasonable efforts will
     be made to avoid placing the element such that it overlaps with
     any bboxes listed in EXCLUSIONS.
 
     """
-    xmax = max(bounds.xmax - bbox.width, bounds.xmin)
-    ymax = max(bounds.ymax - bbox.height, bounds.ymin)
+    xmax = max(bounds.xmax - dim.width, bounds.xmin)
+    ymax = max(bounds.ymax - dim.height, bounds.ymin)
     for n in count(1):
         # Compute random position offset
         x = random.uniform(bounds.xmin, xmax)
         y = random.uniform(bounds.ymin, ymax)
-        new_bbox = BBox(x, x + bbox.width, y, y + bbox.height)
+        new_bbox = BoundingBox(x, x + dim.width, y, y + dim.height)
         if not any(new_bbox.overlaps(excl) for excl in exclusions):
             break
         elif n > max_tries:
@@ -136,6 +234,37 @@ def random_position(bbox, bounds, exclusions=[], max_tries=32):
     return x, y
 
 
+def find_exclusions(elem, transform=None):
+    exclusions = []
+
+    if elem.getparent() is None:
+        base = "/svg:svg/*[not(self::svg:defs)]/descendant-or-self::"
+        is_hidden = ("ancestor::svg:g[@inkscape:groupmode='layer']"
+                     "[contains(@style,'display:none')]")
+        cond = "[not({is_hidden})]".format(is_hidden=is_hidden)
+    else:
+        base = "./descendant-or-self::"
+        cond = ""
+
+    path = '|'.join(base + s + cond for s in [
+        "*[@bh:rat-exclusion]",
+        "svg:use[starts-with(@xlink:href,'#')]",
+        ])
+
+    for el in elem.xpath(path, namespaces=NSMAP):
+        if el.get(BH_RAT_EXCLUSION) is not None:
+            exclusions.append(Element(el).compute_bbox(transform))
+        else:
+            assert el.tag == inkex.addNS('use', 'svg')
+            href = el.get(inkex.addNS('href', 'xlink'))
+            assert href.startswith('#') and len(href) > 1
+
+            local_tfm = composed_transform(Element(el), transform)
+            for node in elem.xpath('//*[@id=$ref_id]', ref_id=href[1:]):
+                exclusions.extend(find_exclusions(node, local_tfm))
+    return exclusions
+
+
 def make_auto_exclusion(bbox):
     excl = inkex.etree.Element(
         inkex.addNS('rect', 'svg'),
@@ -144,7 +273,7 @@ def make_auto_exclusion(bbox):
         width="%f" % bbox.width,
         height="%f" % bbox.height,
         # style='display:none;fill:#9bff00;stroke:#ff0000ff;stroke-width:1px',
-        style='fill:#9bff0035;fill-opacity:0.2;stroke:#ff0000;stroke-width:1px',
+        style='fill:#b3c895;fill-opacity:0.25;stroke:#ff0000;stroke-width:1px',
         )
     excl.set("{%s}rat-exclusion" % NSMAP['bh'], 'auto')
     # "lock" the exclusion
@@ -163,24 +292,23 @@ class HideRats(inkex.Effect):
         svg = self.document.getroot()
         xmax = self.unittouu(svg.attrib['width'])
         ymax = self.unittouu(svg.attrib['height'])
-        return BBox(0, xmax, 0, ymax)
+        return BoundingBox(0, xmax, 0, ymax)
 
     def get_boundary(self):
         document = self.document
         bbox = None
         for el in document.xpath('//*[@bh:rat-boundary]', namespaces=NSMAP):
-            bbox = simpletransform.boxunion(bbox, Rat(el).bbox)
-
+            boundary = Element(el)
+            if bbox is None:
+                bbox = boundary.bbox
+            else:
+                bbox += boundary.bbox
         if bbox is None:
             bbox = self.get_page_boundary()
-        return BBox(bbox)
+        return bbox
 
     def get_exclusions(self):
-        document = self.document
-        exclusions = []
-        for el in document.xpath('//*[@bh:rat-exclusion]', namespaces=NSMAP):
-            exclusions.append(Rat(el).bbox)
-        return exclusions
+        return find_exclusions(self.document.getroot())
 
     @reify
     def exclusion_layer(self):
@@ -212,7 +340,11 @@ class HideRats(inkex.Effect):
         bounds = self.get_boundary()
         exclusions = self.get_exclusions()
 
-        rats = [Rat(el) for el in self.selected.values()]
+        # FIXME: only do this for exclusions which are not visible?
+        # for excl in exclusions:
+        #     self.exclusion_layer.append(make_auto_exclusion(excl))
+
+        rats = [Element(el) for el in self.selected.values()]
         for rat in rats:
             # Add an exclusion rectangle for the current rat position
             if not any(rat.bbox.overlaps(excl) for excl in exclusions):
@@ -220,7 +352,7 @@ class HideRats(inkex.Effect):
             exclusions.append(rat.bbox)
 
         for rat in rats:
-            newpos = random_position(rat.bbox, bounds, exclusions)
+            newpos = random_position(rat.bbox.dimension, bounds, exclusions)
             rat.position = newpos
             self.exclusion_layer.append(make_auto_exclusion(rat.bbox))
             exclusions.append(rat.bbox)
