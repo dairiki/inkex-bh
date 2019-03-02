39459d928f6dbec4c0b6909dc2f7ae3ef751f0a1
Jeff Dairiki <dairiki@dairiki.org>
2019-03-02 11:03:36 -0800

    Run optipng on PNG export, use --export-id=ID if only one selection

--- bh-create-inset.py
+++ bh-create-inset.py
@@ -6,8 +6,8 @@
 import base64
 from contextlib import contextmanager
 import os
-from subprocess import check_call
-import sys
+import subprocess
+import struct
 import tempfile
 
 import inkex
@@ -34,6 +34,14 @@ def temp_fn(*args, **kw):
         os.unlink(fn)
 
 
+def png_dimensions(png_data):
+    assert len(png_data) >= 24
+    assert png_data[:8] == b'\x89PNG\r\n\x1a\n'
+    assert png_data[12:16] == b'IHDR'
+    width, height = struct.unpack(">LL", png_data[16:24])
+    return width, height
+
+
 class CreateInset(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
@@ -46,54 +54,74 @@ class CreateInset(inkex.Effect):
                                      default="#ffffff")
         self.OptionParser.add_option("--background-opacity", type="float",
                                      default=1.0)
+        self.OptionParser.add_option("--optipng-level", type="int",
+                                     default=2)
+        self.OptionParser.add_option("--verbose", type="inkbool",
+                                     default=False)
 
-    def export_png(self, bbox,
-                   dpi=144,
-                   background='#ffffff',
-                   background_opacity=1.0,
-                   verbose=False):
-        stdout = sys.stderr if verbose else open(os.devnull, 'wb')
+    def run(self, cmd, fail_ok=False):
+        try:
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as ex:
+            if ex.output:
+                inkex.errormsg(ex.output)
+            inkex.errormsg(ex)
+            if not fail_ok:
+                raise
+        else:
+            if self.options.verbose:
+                inkex.errormsg(output)
+
+    def export_png(self):
         document = self.document
-        xmin, xmax, ymin, ymax = bbox
-        svg = document.getroot()
-        height = self.unittouu(svg.get('height'))
-        # Flip origins for UL to LL corner
-        x0, y0 = xmin, height - ymax
-        x1, y1 = xmax, height - ymin
+        selected = self.selected
+        opt = self.options
+
+        inkscape_args = [
+            '--export-background', opt.background,
+            '--export-background-opacity', fmt_f(opt.background_opacity),
+            '--export-dpi', fmt_f(opt.scale * opt.dpi),
+            ]
+
+        if len(selected) == 0:
+            inkex.errormsg(_("You must select at least one object."))
+            exit(1)
+        elif len(selected) == 1:
+            inkscape_args.extend([
+                '--export-id', selected.keys()[0],
+                ])
+        else:
+            xmin, xmax, ymin, ymax = simpletransform.computeBBox(
+                selected.values())
+            svg = document.getroot()
+            height = self.unittouu(svg.get('height'))
+            # Flip origins for UL to LL corner
+            x0, y0 = xmin, height - ymax
+            x1, y1 = xmax, height - ymin
+            inkscape_args.extend([
+                '--export-area', ":".join(map(fmt_f, (x0, y0, x1, y1))),
+                ])
 
         with temp_fn(suffix=".svg", prefix="bh-") as infn:
             with open(infn, "w") as fp:
                 document.write(fp)
             with temp_fn(suffix=".png", prefix="bh-") as outfn:
-                cmd = [
-                    'inkscape',
-                    '--export-png', outfn,
-                    '--export-area', ":".join(map(fmt_f, (x0, y0, x1, y1))),
-                    '--export-background', str(background),
-                    '--export-background-opacity', fmt_f(background_opacity),
-                    '--export-dpi', fmt_f(dpi),
-                    infn,
-                    ]
-                check_call(cmd, stdout=stdout)
+                self.run(['inkscape', '--file', infn, '--export-png', outfn]
+                         + inkscape_args)
+                if opt.optipng_level >= 0:
+                    self.run(['optipng',
+                              '-o', str(opt.optipng_level), outfn],
+                             fail_ok=True)
                 with open(outfn, 'rb') as fp:
                     return fp.read()
 
     def effect(self):
-        if len(self.selected) == 0:
-            inkex.errormsg(_("You must select at least one object."))
-            exit(1)
-        bbox = simpletransform.computeBBox(self.selected.values())
+        dpi = self.options.dpi
+        png_data = self.export_png()
+        png_w, png_h = png_dimensions(png_data)
+        width = png_w * 96.0 / dpi
+        height = png_h * 96.0 / dpi
 
-        scale = self.options.scale
-        png_data = self.export_png(
-            bbox,
-            dpi=scale * self.options.dpi,
-            background=self.options.background,
-            background_opacity=self.options.background_opacity)
-
-        xmin, xmax, ymin, ymax = bbox
-        width = scale * (xmax - xmin)
-        height = scale * (ymax - ymin)
         image = inkex.etree.SubElement(
             self.document.getroot(),
             SVG_IMAGE,
@@ -101,12 +129,17 @@ class CreateInset(inkex.Effect):
             height=fmt_f(height),
             x=fmt_f(self.view_center[0] - width / 2),
             y=fmt_f(self.view_center[1] - height / 2),
-            preserveAspectRatio="none",  # FIXME: is this what is wanted?
-            style="image-rendering:optimizeQuality"
+            # XXX: Inkscape normally sets preserveAspectRatio=none
+            # which allows the image to be scaled arbitrarily.
+            # SVG default is preserveAspectRatio=xMidYMid, which
+            # preserve the image aspect ratio on scaling and seems
+            # to make more sense for us.
+            #
+            # preserveAspectRatio="none",
+            style="image-rendering:optimizeQuality",
             )
         image.set(XLINK_HREF,
-                  'data:image/png;base64,' + base64.encodestring(
-                      png_data).rstrip())
+                  'data:image/png;base64,' + base64.b64encode(png_data))
 
 
 if __name__ == '__main__':
