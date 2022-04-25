990ad1e5b999ef816b33d94ff53e95ee265c7b5a
Jeff Dairiki <dairiki@dairiki.org>
2022-04-25 16:50:41 -0700

    Use local config for testing latest version of Inkscape

---
+++ inkscape.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+: ${INKSCAPE:=/home/dairiki/Downloads/Inkscape-0a00cf5-x86_64.AppImage}
+
+HERE=${0%/*}
+: ${HERE:=$PWD}
+
+export XDG_CONFIG_HOME="${HERE}/test-config"
+
+exec "$INKSCAPE" "$@"
