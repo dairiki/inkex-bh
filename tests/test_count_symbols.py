08589d4866a745c43255e8400238bf8f45d7f367
Jeff Dairiki <dairiki@dairiki.org>
2022-08-19 15:20:16 -0700

    Use svg_maker in more tests

--- tests/test_count_symbols.py
+++ tests/test_count_symbols.py
@@ -2,14 +2,10 @@
 import re
 from collections import Counter
 
-import inkex
 import pytest
-from lxml import etree
 
 from inkex_bh.count_symbols import count_symbols
 from inkex_bh.count_symbols import CountSymbols
-
-pytestmark = pytest.mark.usefixtures("assert_no_stdout")
 
 
 @pytest.fixture
@@ -17,105 +13,71 @@ def effect():
     return CountSymbols()
 
 
-@pytest.fixture
-def svg():
-    tree = inkex.load_svg(
-        """
-    <svg
-       viewBox="0 0 816 1056"
-       id="svg5"
-       sodipodi:docname="test1.svg"
-       xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-       xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-       xmlns:xlink="http://www.w3.org/1999/xlink"
-       xmlns="http://www.w3.org/2000/svg"
-       xmlns:svg="http://www.w3.org/2000/svg">
-      <sodipodi:namedview
-         id="namedview7"
-         a-bunch-of-stuff-elided="..."
-         inkscape:current-layer="layer1" />
-      <defs
-         id="defs2">
-        <svg:symbol id="sym1" />
-        <svg:symbol id="sym2" />
-      </defs>
-      <g
-         inkscape:label="Layer 1"
-         inkscape:groupmode="layer"
-         id="layer1" />
-    </svg>
-    """
-    )
-    return tree.getroot()
+pytestmark = pytest.mark.usefixtures("assert_quiet")
 
 
-def test_count_symbols(svg):
-    sym1, sym2 = svg.xpath("//svg:symbol")[:2]
-    layer = svg.findone("./svg:g[@inkscape:groupmode='layer']")
-    layer.append(inkex.Use.new(sym1.eid, 1, 1))
-    layer.append(inkex.Use.new(sym2.eid, 2, 2))
-    layer.append(inkex.Use.new(sym2.eid, 3, 3))
+def test_count_symbols(svg_maker):
+    sym1 = svg_maker.add_symbol()
+    sym2 = svg_maker.add_symbol()
+    svg_maker.add_use(sym1)
+    svg_maker.add_use(sym2, x=2)
+    svg_maker.add_use(sym2, x=3)
 
+    svg = svg_maker.svg
     assert count_symbols(svg.xpath("//svg:use")) == Counter(
         {
-            f"#{sym1.eid}": 1,
-            f"#{sym2.eid}": 2,
+            f"#{sym1.get('id')}": 1,
+            f"#{sym2.get('id')}": 2,
         }
     )
 
 
-def test_count_symbols_in_groups(svg):
-    sym = svg.findone("//svg:symbol")
+def test_count_symbols_in_groups(svg_maker):
+    sym = svg_maker.add_symbol()
 
-    layer = svg.findone("./svg:g[@inkscape:groupmode='layer']")
-    group1 = inkex.Group.new(
-        "test", inkex.Use.new(sym.eid, 0, 0), inkex.Use.new(sym.eid, 1, 1)
-    )
-    layer.append(group1)  # 2 sym
-    group2 = inkex.Group.new(
-        "test", inkex.Use.new(group1.eid, 0, 0), inkex.Use.new(sym.eid, 1, 1)
-    )
-    layer.append(group2)  # 2 + 1 = 3 syms
+    # two symbols in a group
+    group1 = svg_maker.add_group()
+    svg_maker.add_use(sym, x=1, parent=group1)
+    svg_maker.add_use(sym, x=2, parent=group1)
 
-    # group1 is visible with two syms
-    layer.append(inkex.Use.new(group1.eid, 10, 10))  # 2 syms
-    layer.append(inkex.Use.new(group2.eid, 20, 20))  # 3 syms
-    layer.append(inkex.Use.new(sym.eid, 30, 30))  # 1 syms
+    # another group, with one symbol directly and two more indirectly
+    group2 = svg_maker.add_group()
+    svg_maker.add_use(sym, x=3, parent=group2)
+    svg_maker.add_use(group1, x=4, parent=group2)
 
-    counts = count_symbols(svg.xpath("//svg:use"))
-    assert counts == Counter({f"#{sym.eid}": 11})
+    svg_maker.add_use(group1, y=1)  # 2 more indirect syms
+    svg_maker.add_use(group2, y=2)  # 3 more indirect syms
+    svg_maker.add_use(sym, y=3)  # 1 direct sym
+
+    svg = svg_maker.svg
+    assert count_symbols(svg.xpath("//svg:use")) == Counter({f"#{sym.get('id')}": 11})
 
 
-def test_count_symbols_warn_on_missing_href(svg, capsys):
-    layer = svg.findone("./svg:g[@inkscape:groupmode='layer']")
-    layer.append(inkex.Use.new("missing-ref", 10, 10))
-
+def test_count_symbols_warn_on_missing_href(svg_maker, capsys):
+    svg_maker._add("svg:use")
+    svg = svg_maker.svg
     counts = count_symbols(svg.xpath("//svg:use"))
     assert counts == Counter()
-    print("foo")
     output = capsys.readouterr()
     assert re.search(r"WARNING\b.*\bno element for href\b", output.err)
+    assert output.out == ""
 
 
-def test_effect(svg, run_effect, tmp_path, capsys):
-    sym1, sym2 = svg.xpath("//svg:symbol")[:2]
-    layer = svg.findone("./svg:g[@inkscape:groupmode='layer']")
-    layer.append(inkex.Use.new(sym1.eid, 10, 10))
+def test_effect(svg_maker, run_effect, tmp_path, capsys):
+    sym1 = svg_maker.add_symbol()
+    svg_maker.add_use(sym1)
 
-    hidden = inkex.Layer.new("hidden layer")
-    hidden.style["display"] = "none"
-    hidden.append(inkex.Use.new(sym2.eid, 20, 20))
-    svg.append(hidden)
+    sym2 = svg_maker.add_symbol()
+    hidden = svg_maker.add_layer("Hidden", visible=False)
+    svg_maker.add_use(sym2, parent=hidden)
 
-    infile = tmp_path / "in.svg"
-    infile.write_bytes(etree.tostring(svg))
-
-    assert run_effect(infile) is None  # no output
-    output = capsys.readouterr().err
+    assert run_effect(svg_maker.as_file()) is None  # no output
+    output = capsys.readouterr()
+    assert output.out == ""
     matches = {
         m.group("symbol"): int(m.group("count"))
         for m in re.finditer(
-            r"(?mx)^ \s* (?P<count>\d+): \s+ (?P<symbol>\S+) $", output
+            r"(?mx)^ \s* (?P<count>\d+): \s+ (?P<symbol>\S+) $", output.err
         )
     }
-    assert matches == {f"#{sym1.eid}": 1}
+    assert matches == {f"#{sym1.get('id')}": 1}
--- tests/test_random_seed.py
+++ tests/test_random_seed.py
@@ -2,8 +2,6 @@
 # mypy: ignore-errors
 from __future__ import annotations
 
-import inspect
-
 import inkex
 import pytest
 
@@ -12,72 +10,27 @@ from inkex_bh.constants import NSMAP
 from inkex_bh.random_seed import RandomSeed
 
 
-TEST_SVG_TEMPL = inspect.cleandoc(
-    """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <svg
-       width="8.5in"
-       height="11in"
-       viewBox="0 0 816 1056"
-       version="1.1"
-       id="svg5"
-       sodipodi:docname="test.svg"
-       xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-       xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-       xmlns="http://www.w3.org/2000/svg"
-       xmlns:svg="http://www.w3.org/2000/svg"
-       {extra_root_attrs}>
-      <sodipodi:namedview
-         id="namedview7"
-         a-bunch-of-stuff-elided="..."
-         inkscape:current-layer="layer1" />
-      <defs
-         id="defs2" />
-      <g
-         inkscape:label="Layer 1"
-         inkscape:groupmode="layer"
-         id="layer1" />
-    </svg>
-    """
-)
-
-
 @pytest.fixture
 def effect() -> inkex.InkscapeExtension:
     return RandomSeed()
 
 
-@pytest.fixture
-def random_seed():
-    return None
+pytestmark = pytest.mark.usefixtures("assert_quiet")
 
 
-@pytest.fixture
-def test_svg(tmp_path, random_seed):
-    extra_root_attrs = ""
-    if random_seed is not None:
-        extra_root_attrs = f'bh:random-seed="{random_seed}" xmlns:bh="{NSMAP["bh"]}"'
-
-    test_svg = tmp_path / "test.svg"
-    test_svg.write_text(TEST_SVG_TEMPL.format(extra_root_attrs=extra_root_attrs))
-    return test_svg
-
-
-pytestmark = pytest.mark.usefixtures("assert_no_stdout")
-
-
-def test_adds_seed(test_svg, run_effect):
-    out_svg = run_effect(test_svg)
+def test_adds_seed(svg_maker, run_effect):
+    out_svg = run_effect(svg_maker.as_file())
     assert out_svg.get(BH_RANDOM_SEED).isdigit()
 
 
-def test_adds_ns_decl(test_svg, run_effect):
-    out_svg = run_effect(test_svg)
+def test_adds_ns_decl(svg_maker, run_effect):
+    out_svg = run_effect(svg_maker.as_file())
     assert out_svg.nsmap["bh"] == NSMAP["bh"]
 
 
-@pytest.mark.parametrize("random_seed", [42])
-def test_leaves_existing_seed(test_svg, run_effect, capsys):
-    svg = run_effect(test_svg)
+def test_leaves_existing_seed(svg_maker, run_effect, capsys):
+    svg_maker.svg.set(BH_RANDOM_SEED, "42")
+    svg = run_effect(svg_maker.as_file())
     assert svg is None
     output = capsys.readouterr()
     assert output.out == ""
