ff2cc1f2608831e185c443affca3ba20651acf84
Jeff Dairiki <dairiki@dairiki.org>
2022-10-07 17:31:48 -0700

    Fix for Windows

--- hatch_build.py
+++ hatch_build.py
@@ -7,8 +7,8 @@ from hatchling.metadata.plugin.interface import MetadataHookInterface
 
 class ReadmeMetadataHook(MetadataHookInterface):
     def update(self, metadata: Dict[str, Any]) -> None:
-        readme = Path(self.root, "README.md").read_text()
-        changes = Path(self.root, "CHANGES.md").read_text()
+        readme = Path(self.root, "README.md").read_text("utf-8")
+        changes = Path(self.root, "CHANGES.md").read_text("utf-8")
 
         metadata["readme"] = {
             "content-type": "text/markdown",
